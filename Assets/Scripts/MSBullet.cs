73d7077fa2206c51db4e4fbabacbb1bf9b84cc10
555isfaiz <1244274837@qq.com>
2022-06-20 16:13:26 +0200

    fix bullet direction problem && change boundary collider

--- Assets/Scripts/MSBullet.cs
+++ Assets/Scripts/MSBullet.cs
@@ -3,15 +3,21 @@ using UnityEngine;
 public class MSBullet : MonoBehaviour
 {
     MonoBehaviour shooter;
+    float defaultSpeed = MSGlobalParams.Rifle_bullet_fly_speed;
 
     void FixedUpdate()
     {
-        gameObject.transform.Translate(gameObject.transform.forward * MSGlobalParams.Rifle_bullet_fly_speed * Time.deltaTime);
+        gameObject.transform.Translate(Vector3.forward * defaultSpeed * Time.deltaTime, gameObject.transform);
     }
 
     void SetShooter(MonoBehaviour shooter)
     {
         this.shooter = shooter;
+    }
+
+    void SetSpeed(float speed)
+    {
+        this.defaultSpeed = speed;
     }
 
     void OnCollisionEnter(Collision collision)
